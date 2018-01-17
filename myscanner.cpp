289a6da9cddd823a8106f4495fbb9dba9dd58e50
Sanit <ankitsalvi233@gmail.com>
2018-01-17 23:29:43 +0530

    writting token phase 1

--- myscanner.cpp
+++ myscanner.cpp
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<cstring>
 #include "myscanner.h"
+#include <iostream>
+#include <sstream>
 
 extern int yylex();
 extern char* yytext;
@@ -10,21 +12,38 @@ int main()
 {
 	int ntoken;
 	ntoken=yylex();
-	const char *arr[]={NULL,"OPEN_BRACE","CLOSE_BRACE","OPEN_CIRCULAR_BRACE","CLOSE_CIRCULAR_BRACE","COLON","IDENTIFIER","INTERGER"};
-	int count[8];
+
+	const char *arr[]={NULL,"OPEN_BRACE","CLOSE_BRACE","OPEN_CIRCULAR_BRACE","CLOSE_CIRCULAR_BRACE","COLON","INT","IDENTIFIER","INTERGER"};
+	int count[9];
 	std::memset(count,0,sizeof(count));
 	while(ntoken)
 	{	
-		count[ntoken]++;		
-		//printf("%s|%d\n",arr[ntoken],ntoken);
-		ntoken=yylex();
+	count[ntoken]++;		
+	//printf("%s|%d\n",arr[ntoken],ntoken);
+	ntoken=yylex();
 	}
 	int i=1;
-	while(i<8)
+	while(i<9)
 	{
-		printf("%s|%d\n",arr[i],count[i]);
-		i++;	
+	printf("%s|%d\n",arr[i],count[i]);
+	i++;	
 	}
 	return 0;
+	 
 
+/*	std::istringstream  data("some string or input file");
+	yyFlexLexer l(&data, &std::cout);
+
+
+	while (l.yylex())
+	{
+		std::cout << std::string(l.YYText(), l.YYLeng()) << "\n";
+	}
+
+
+	return 0;
+*/
+
+
+	
 }
--- myscanner.h
+++ myscanner.h
@@ -1,8 +1,67 @@
-#define OPEN_BRACE 1
-#define CLOSE_BRACE 2
-#define OPEN_CIRCULAR_BRACE 3
-#define CLOSE_CIRCULAR_BRACE 4
-#define COLON 5
-#define IDENTIFIER 6
-#define INTERGER 7
+#define FOR 1
+#define	WHILE 2
+#define IF 3
+#define ELSE 4
+#define THROW 5
+#define IMPLEMENTS 6
+#define ABSTRACT 7
+#define CONTINUE 8
+#define THIS 9
+#define SWITCH 10
+#define CASE 11
+#define DO 12
+#define ASSERT 13
+#define	PROTECTED 14
+#define	PRIVATE 15
+#define PUBLIC 16
+#define RETURN 17
+#define	EXTENDS 18
+#define	BOOLEAN 19
+#define	STATIC 20
+#define	VOID 21
+#define CONST 22
+#define	CLASS 23
+#define CATCH 24
+#define	TRY 25
+#define NEW 26
+#define	IMPORT 27
+#define  28
+#define	BREAK 29
+#define INT  30
+#define CHAR 31
+#define FLOAT 33
+#define LONG 34
+#define DOUBLE 35
+#define ENUM 36
+#define SHORT 37
+
+
+
+
+#define OPEN_CURL_BRACE 38
+#define CLOSE_CURL_BRACE 39
+#define OPEN_CIRCULAR_BRACE 40 
+#define CLOSE_CIRCULAR_BRACE 41
+#define OPEN_SQUARE_BRACKET 42
+#define CLOSE_SQUARE_BRACKET 43
+#define COMMA 44
+#define SEMICOLON 45
+#define DOT 46
+
+#define ASSIGNMENT 47
+#define PLUS 48
+#define MINUS 49
+#define FORWARDSLASH 50
+#define STAR 51
+#define EQUALS 52
+#define NOTEQUAL 53
+#define AND 54
+#define OR 55
+
+#define IDENTIFIER 56
+#define INT_CONT 57
+#define FLOAT_CONST 58
+#define WHITESPACE 59
+
+
 
