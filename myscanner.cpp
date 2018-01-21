49ad5580778fb96fe7fbd5c1bf11f09ae7a88a92
dheerajathrey <dheeraj.athrey@gmail.com>
2018-01-21 22:42:56 +0530

    array updated, serial numbers updated, working fine

--- myscanner.cpp
+++ myscanner.cpp
@@ -13,8 +13,8 @@ int main()
 	int ntoken;
 	ntoken=yylex();
 
-	const char *arr[]={NULL,"OPEN_BRACE","CLOSE_BRACE","OPEN_CIRCULAR_BRACE","CLOSE_CIRCULAR_BRACE","COLON","INT","IDENTIFIER","INTERGER"};
-	int count[9];
+	const char *arr[]={NULL, "FOR", "WHILE", "IF", "ELSE", "THROW", "CONTINUE", "THIS", "SWITCH", "CASE", "DO", "ASSERT", "PUBLIC", "RETURN", "EXTENDS", "STATIC", "FINAL", "VOID", "CONST", "CLASS", "CATCH", "TRY", "NEW", "IMPORT", "BREAK", "INT", "CHAR", "FLOAT","LONG","DOUBLE","SHORT", "BYTE", "BOOLEAN", "SUPER", "ENUM", "OPEN_BRACE", "CLOSE_BRACE", "OPEN_CIRCULAR_BASE", "CLOSE_CIRCULAR_BASE", "OPEN_SQUARE_BRACKET", "CLOSE_SQUARE_BRACKET", "COMMA", "SEMICOLON", "DOT", "ASSIGNMENT", "INTEGER", "IDENTIFIER"};
+	int count[47];
 	std::memset(count,0,sizeof(count));
 	while(ntoken)
 	{	
@@ -23,7 +23,7 @@ int main()
 	ntoken=yylex();
 	}
 	int i=1;
-	while(i<9)
+	while(i<47)
 	{
 	printf("%s|%d\n",arr[i],count[i]);
 	i++;	
--- myscanner.h
+++ myscanner.h
@@ -3,61 +3,64 @@
 #define IF 3
 #define ELSE 4
 #define THROW 5
-#define CONTINUE 8
-#define THIS 9
-#define SWITCH 10
-#define CASE 11
-#define DO 12
-#define ASSERT 13
-#define PUBLIC 16
-#define RETURN 17
-#define	EXTENDS 18
-#define	BOOLEAN 19
-#define	STATIC 20
-#define	VOID 21
-#define CONST 22
-#define	CLASS 23
-#define CATCH 24
-#define	TRY 25
-#define NEW 26
-#define	IMPORT 27
-#define  28
-#define	BREAK 29
-#define INT  30
-#define CHAR 31
-#define FLOAT 33
-#define LONG 34
-#define DOUBLE 35
-#define ENUM 36
-#define SHORT 37
+#define CONTINUE 6
+#define THIS 7
+#define SWITCH 8
+#define CASE 9
+#define DO 10
+#define ASSERT 11
+#define PUBLIC 12
+#define RETURN 13
+#define	EXTENDS 14
+#define STATIC 15
+#define FINAL 16
+#define	VOID 17
+#define CONST 18
+#define	CLASS 19
+#define CATCH 20
+#define	TRY 21
+#define NEW 22
+#define	IMPORT 23
+#define	BREAK 24
+#define INT  25
+#define CHAR 26
+#define FLOAT 27
+#define LONG 28
+#define DOUBLE 29
+#define SHORT 30
+#define BYTE 31
+#define BOOLEAN 32
+#define SUPER 33
+#define ENUM 34
 
 
 
+#define OPEN_BRACE 35
+#define CLOSE_BRACE 36
+#define OPEN_CIRCULAR_BRACE 37 
+#define CLOSE_CIRCULAR_BRACE 38
+#define OPEN_SQUARE_BRACKET 39
+#define CLOSE_SQUARE_BRACKET 40
+#define COMMA 41
+#define SEMICOLON 42
+#define DOT 43
 
-#define OPEN_CURL_BRACE 38
-#define CLOSE_CURL_BRACE 39
-#define OPEN_CIRCULAR_BRACE 40 
-#define CLOSE_CIRCULAR_BRACE 41
-#define OPEN_SQUARE_BRACKET 42
-#define CLOSE_SQUARE_BRACKET 43
-#define COMMA 44
-#define SEMICOLON 45
-#define DOT 46
+#define ASSIGNMENT 44
+#define INTEGER 45
 
-#define ASSIGNMENT 47
-#define PLUS 48
-#define MINUS 49
-#define FORWARDSLASH 50
-#define STAR 51
-#define EQUALS 52
-#define NOTEQUAL 53
-#define AND 54
-#define OR 55
+#define PLUS 123
+#define MINUS 123
+#define FORWARDSLASH 147
+#define STAR 481
+#define EQUALS 491
+#define NOTEQUAL 501
+#define AND 511
+#define OR 521
 
-#define IDENTIFIER 56
-#define INT_CONT 57
-#define FLOAT_CONST 58
-#define WHITESPACE 59
+#define IDENTIFIER 46
+#define INT_CONT 541
+#define FLOAT_CONST 515
+#define WHITESPACE 561
 
 
 
