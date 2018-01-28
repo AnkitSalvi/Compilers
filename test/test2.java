62c83da79c705c8c32c83b8c2fd2d1370ed7690f
dheerajathrey <dheeraj.athrey@gmail.com>
2018-01-28 06:51:15 +0530

    added test cases test/test2.java and test/test3.java and sent the errors found on fb

---
+++ test/test2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class A {
+
+    public static void main(String[] args) {
+
+        double[] values = { 0.1, 5.2, 5.3, 6.3, 83.3 };
+
+        for(int i=0; i < values.length; i=i+1) {
+            System.out.printf("%.2f ", values[i]);
+        }
+
+        int j=0;
+        while(j > 0){
+        	j+= 2;
+        	if(j==20){
+        		break;
+        	}
+        	else{
+        		j-=1;
+        	}
+        }
+
+        System.out.println("I '\"\"am");
+
+        float a = 33.3;
+        float b = 43.6;
+        float c = a + b;
+
+        boolean p;
+
+        p = (1||0) && (1||1);
+
+        if (!p){
+        	float d = a - b;
+        }
+
+
+
+
+
+    }
+
+} 
---
+++ test/test3.java
@@ -0,0 +1,39 @@
+public class A {
+
+    public static void main(String[] args) {
+
+
+    	int a, b, c, d, e, f, g, h, i, j;
+// we defined 10 integer variables "364839 'f893!!78''\*~"
+    	char u = 'ux';
+    	char v ='v';
+
+    	long int t = 1000000;
+
+    	a = 10; 
+    	b = 4; 
+    	c = -4; 
+    	d =2;
+
+    	e = d * b;
+    	f = a/b;
+    	g = a % d;
+
+    	b*=2;
+    	c/=1;
+
+
+    	if((a >= e) ||(b*c <= a)){
+    		continue;
+    	} 
+
+   		try{  
+      		int h = 50/0;  
+    		}
+    	catch(ArithmeticException e){
+    		System.out.println(e);
+    	}  
+
+
+    }
+} 
